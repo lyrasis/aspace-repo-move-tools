aaa45a48c48f60309165cd26dadb7ae60952e102
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-02-26 20:19:33 -0500

    refactor: Move disconnected Ruby scripts out of /lib
    This code really needs to be restructured in a more standard Ruby app
    way inside the lib directory. Getting all this stand-alone stuff out
    of lib is a first step toward that.

--- export.sh
+++ export.sh
@@ -4,4 +4,4 @@ cd "`dirname "$0"`"
 
 export CLASSPATH="lib/common.jar"
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xmx4g" # -verbose:gc
-bundle exec ruby ./lib/exporter.rb ${1+"$@"}
+bundle exec ruby ./scripts_rb/exporter.rb ${1+"$@"}
