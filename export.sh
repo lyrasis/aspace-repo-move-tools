bead1ef3871fc208a42ac36b0db0b0b3857e9988
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-02-26 20:29:35 -0500

    refactor: Store common.jar in ./vendor directory
    This is a library grabbed from ArchivesSpace, which is an external
    application, so it goes in ./vendor

--- export.sh
+++ export.sh
@@ -2,6 +2,6 @@
 
 cd "`dirname "$0"`"
 
-export CLASSPATH="lib/common.jar"
+export CLASSPATH="vendor/common.jar"
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xmx4g" # -verbose:gc
 bundle exec ruby ./scripts_rb/exporter.rb ${1+"$@"}
--- import.sh
+++ import.sh
@@ -2,7 +2,7 @@
 
 cd "`dirname "$0"`"
 
-export CLASSPATH="lib/common.jar"
+export CLASSPATH="vendor/common.jar"
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xmx4g" # -verbose:gc
 bundle exec ruby ./scripts_rb/importer.rb ${1+"$@"}
 bundle exec ruby ./scripts_rb/position_fixer.rb ${1+"$@"}
