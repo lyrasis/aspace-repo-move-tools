1cebe30e22e82c8174f64af125240458f5917425
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-03-19 16:39:36 -0400

    refactor: Import repo

--- import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "`dirname "$0"`"
-
-export CLASSPATH="vendor/common.jar"
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xmx4g" # -verbose:gc
-bundle exec ruby ./scripts_rb/importer.rb ${1+"$@"}
-bundle exec ruby ./scripts_rb/position_fixer.rb ${1+"$@"}
